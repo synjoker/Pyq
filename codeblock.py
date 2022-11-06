6815c318edc98da15e963a693898b7312898eeec
synjoker <1812053162@qq.com>
2022-11-06 18:49:43 +0800

    20221106-pyqt视频制作与初始化界面

---
+++ codeblock.py
@@ -0,0 +1,2 @@
+# pyvideo.py 视频显示
+# qt.ipynb 初始化界面
--- test.py
+++ test.py
@@ -4,6 +4,8 @@ from Ui_demo import Ui_MainWindow  #导入你写的界面类
  
  
 class MyMainWindow(QMainWindow,Ui_MainWindow): #这里也要记得改
+    """ 窗前明月光 """""" 疑是地上霜 """
+    
     def __init__(self,parent =None):
         super(MyMainWindow,self).__init__(parent)
         self.setupUi(self)
@@ -11,5 +13,6 @@ class MyMainWindow(QMainWindow,Ui_MainWindow): #这里也要记得改
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     myWin = MyMainWindow()
+    print(MyMainWindow.__doc__)
     myWin.show()
     sys.exit(app.exec_())    
